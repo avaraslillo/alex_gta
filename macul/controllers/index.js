f2be52f1420575d9185f33597b8b8904e15b7fda
avaraslillo <a.varaslillo@hotmail.com>
2016-09-09 13:12:31 -0300

    Update index.js

--- macul/controllers/index.js
+++ macul/controllers/index.js
@@ -12,5 +12,10 @@ module.exports = function (router) {
         res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
         
     });
+    
+    router.post('/', function (req,res){
+        var x=req.body;
+        res.send('<code><pre>' + req.body + '<pre></code>');
+    });
 
 };
